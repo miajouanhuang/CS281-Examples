40a2f17ff99f33c19d329c9f77c5c4c5d50031b5
miajouanhuang <miahuang1988@gmail.com>
2017-04-18 15:04:24 +0800

    add Lecture02

--- Lecture02/Example02/Program.cs
+++ Lecture02/Example02/Program.cs
@@ -10,8 +10,14 @@ namespace Example02
     {
         private static void Main(string[] args)
         {
-            List<string> cart = new List<string>();
-            cart.Add("product id");
+            Console.Write("請輸入共要幾個數字:");
+            int length = int.Parse(Console.ReadLine());
+            int[] numbers = new int[length];
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                Console.Write("請輸入第{0}個數字:", index + 1);
+                numbers[index] = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
